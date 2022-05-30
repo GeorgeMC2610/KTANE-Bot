eac85e47b53e5701a66d1a05568219aaf2e65059
Giorgos Seimenis <giorgosseimenis@gmail.com>
2022-05-30 16:06:52 +0300

    stage 1 and 2 done

--- Bomb/Memory.cs
+++ Bomb/Memory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace KTANE_Bot
@@ -6,13 +7,11 @@ namespace KTANE_Bot
     {
         public int Stage { get; private set; }
 
-        private int _display;
-        private int _firstNumber;
-        private int _secondNumber;
-        private int _thirdNumber;
-        private int _fourthNumber;
+        private int[] _sequence;
+        private int[] _positions;
+        private int[] _numbers;
 
-        private static Dictionary<string, int> _numbersDict = new Dictionary<string, int>
+        private static readonly Dictionary<string, int> NumbersDict = new Dictionary<string, int>
         {
             { "one", 1 },
             { "two", 2 },
@@ -23,6 +22,9 @@ namespace KTANE_Bot
         public Memory(Bomb bomb) : base(bomb)
         {
             Stage = 1;
+            _positions = new int[5];
+            _numbers = new int[5];
+            _sequence = new int[5];
         }
 
         public override string Solve()
@@ -31,8 +33,69 @@ namespace KTANE_Bot
             
             switch (Stage)
             {
+                case 1:
+                    switch (_sequence[0])
+                    {
+                        case 1:
+                        case 2:
+                            _positions[0] = 2;
+                            _numbers[0] = _sequence[2];
+                            numberToPress = _sequence[2];
+                            break;
+                        case 3:
+                            _positions[0] = 3;
+                            _numbers[0] = _sequence[3];
+                            numberToPress = _sequence[3];
+                            break;
+                        default:
+                            _positions[0] = 4;
+                            _numbers[0] = _sequence[4];
+                            numberToPress = _sequence[4];
+                            break;
+                    }
+                    
+                    break;
+                
+                case 2:
+                    switch (_sequence[0])
+                    {
+                        case 1:
+                            _positions[1] = Array.IndexOf(_sequence, 4);
+                            _numbers[1] = 4;
+                            numberToPress = 4;
+                            break;
+                        case 2:
+                            _positions[1] = _positions[0];
+                            _numbers[1] = _sequence[_positions[0]];
+                            numberToPress = _sequence[_positions[0]];
+                            break;
+                        case 3:
+                            _positions[1] = 1;
+                            _numbers[1] = _sequence[1];
+                            numberToPress = _sequence[1];
+                            break;
+                        default:
+                            _positions[1] = _positions[0];
+                            _numbers[1] = _sequence[_positions[0]];
+                            numberToPress = _sequence[_positions[0]];
+                            break;
+                    }
+
+                    break;
+                
+                case 3:
+
+                    if (_positions[0] == 1)
+                    {
+                        
+                    }
+
+                    break;
                 
             }
+
+            Stage++;
+            return $"Press {numberToPress}";
         }
 
         public bool SetNumbers(params string[] numbers)
@@ -42,11 +105,11 @@ namespace KTANE_Bot
             
             try
             {
-                _display = _numbersDict[numbers[1]];
-                _firstNumber = _numbersDict[numbers[2]];
-                _secondNumber = _numbersDict[numbers[3]];
-                _thirdNumber = _numbersDict[numbers[4]];
-                _fourthNumber = _numbersDict[numbers[5]];
+                _sequence[0] = NumbersDict[numbers[1]];
+                _sequence[1] = NumbersDict[numbers[2]];
+                _sequence[2] = NumbersDict[numbers[3]];
+                _sequence[3] = NumbersDict[numbers[4]];
+                _sequence[4] = NumbersDict[numbers[5]];
                 return true;
             }
             catch (KeyNotFoundException)
