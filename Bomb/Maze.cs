9c0c4df8aee053530fc189e69fcdc699b55aa71f
Giorgos Seimenis <giorgosseimenis@gmail.com>
2022-05-30 14:20:04 +0300

    added maze (this is gonna be fun)

---
+++ Bomb/Maze.cs
@@ -0,0 +1,14 @@
+namespace KTANE_Bot
+{
+    public class Maze : KTANE_Module
+    {
+        internal Maze(Bomb bomb) : base(bomb)
+        {
+        }
+
+        public override string Solve()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
