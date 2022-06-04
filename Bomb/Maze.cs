bf290bacdc2ed873b0829a97520381df17ba412b
Giorgos Seimenis <giorgosseimenis@gmail.com>
2022-06-04 21:10:09 +0300

    this is bfs????

--- Bomb/Maze.cs
+++ Bomb/Maze.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using static KTANE_Bot.Maze.Block;
 
 namespace KTANE_Bot
@@ -13,6 +14,7 @@ namespace KTANE_Bot
             public bool Up { get; }
             public bool Right { get; }
             public bool Left { get; }
+            public bool Marked { get; set; }
 
             public Block(bool up, bool down, bool right, bool left)
             {
@@ -20,6 +22,7 @@ namespace KTANE_Bot
                 Down = down;
                 Right = right;
                 Left = left;
+                Marked = false;
             }
 
             public static readonly Block Nothing = new Block(false, false, false, false);
@@ -56,25 +59,82 @@ namespace KTANE_Bot
         private Point _squareLocation;
         private Point _triangleLocation;
         private Queue<Point> _visitedPoints;
+        private int[] _circles;
 
         public Maze(Bomb bomb) : base(bomb)
         {
             _visitedPoints = new Queue<Point>();
+            _circles = new[] {-1, -1, -1, -1};
         }
 
         public override string Solve()
         {
-            return "";
+            if (_squareLocation.IsEmpty || _triangleLocation.IsEmpty)
+                return @"Something is wrong.";
+
+            _visitedPoints.Enqueue(_triangleLocation);
+            Maze1[_triangleLocation.X, _triangleLocation.Y].Marked = true;
+            var moves = new List<Point>();
+
+            while (_visitedPoints.Count != 0)
+            {
+                var cell = _visitedPoints.Peek();
+                moves.Add(cell);
+                
+                _visitedPoints.Dequeue();
+                
+                //move right (this is y+1)
+                if (cell.Y + 1 < 6 && !Maze1[cell.X, cell.Y + 1].Marked && Maze1[cell.X, cell.Y + 1].Right)
+                {
+                    _visitedPoints.Enqueue(new Point(cell.X, cell.Y + 1));
+                    Maze1[cell.X, cell.Y + 1].Marked = true;
+                }
+                
+                //move down (this is (x+1)
+                if (cell.X + 1 < 6 && !Maze1[cell.X + 1, cell.Y].Marked && Maze1[cell.X + 1, cell.Y].Down)
+                {
+                    _visitedPoints.Enqueue(new Point(cell.X + 1, cell.Y));
+                    Maze1[cell.X + 1, cell.Y].Marked = true;
+                }
+                
+                //move left (this is y-1)
+                if (cell.Y - 1 >= 0 && !Maze1[cell.X, cell.Y - 1].Marked && Maze1[cell.X, cell.Y - 1].Left)
+                {
+                    _visitedPoints.Enqueue(new Point(cell.X, cell.Y - 1));
+                    Maze1[cell.X, cell.Y - 1].Marked = true;
+                }
+                
+                //move up (this is x-1)
+                if (cell.X - 1 >= 0 && !Maze1[cell.X - 1, cell.Y].Marked && Maze1[cell.X, cell.Y].Up)
+                {
+                    _visitedPoints.Enqueue(new Point(cell.X - 1, cell.Y));
+                    Maze1[cell.X - 1, cell.Y].Marked = true;
+                }
+            }
+            
+            
         }
 
         public void AssignFirstCircle(int x, int y)
         {
-            
+            _circles[0] = --x;
+            _circles[1] = --y;
         }
 
         public void AssignSecondCircle(int x, int y)
         {
-            
+            _circles[2] = --x;
+            _circles[3] = --y;
+        }
+
+        public void SetTriangle(int x, int y)
+        {
+            _triangleLocation = new Point(--x, --y);
+        }
+
+        public void SetSquare(int x, int y)
+        {
+            _squareLocation = new Point(--x, --y);
         }
     }
 }
